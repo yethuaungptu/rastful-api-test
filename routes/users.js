fac3fa1b50fb62cda5100c1912812d65f9f9ea06
yethuaungptu <yethuaung481995@gamil.com>
2018-08-28 14:02:43 +0630

    add user add

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,23 @@ router.get('/', function(req, res, next) {
 router.get('/add', function(req, res, next) {
   res.json({msg:'please enter user field',fields_name:'name,phone,job,email,password'})
 });
+
+router.post('/add',(req,res,next)=>{
+  var user = new User();
+  user.name = req.body.name;
+  user.phone = req.body.phone;
+  user.job = req.body.job;
+  user.email = req.body.email;
+  user.password = req.body.password;
+
+  user.save((err,user)=>{
+    if(err) throw err;
+    if(user) {
+      res.json({msg:'Succefully add',data:user});
+    }else{
+      res.json({msg:"Some thing was worng"});
+    }
+  })
+})
 
 module.exports = router;
